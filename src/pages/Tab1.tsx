13da73fd6af7282c2d96894acaaf8587f5980413
unknown <CIAC@CIAC1-020.academic.mixcoac.upmx.mx>
2024-05-21 09:53:59 -0600

    xd

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonButton } from '@ionic/react';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel} from '@ionic/react';
+import { calendar, personCircle, map, informationCircle } from 'ionicons/icons';
+import { IonButtons, IonMenu, IonMenuButton } from '@ionic/react';
+import {getDatabase,ref,push} from "firebase/database"
+import React, { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
+
 import './Tab1.css';
 
 
 const Tab1: React.FC = () => {
+
+  const [titulo, setTitulo] = useState("");
+  const [subtitulo, setSubtitulo] = useState("");
+  const [mensaje, setMensaje] = useState("");
+  const [alerta, setAlerta] = useState(false);
+  
+  const agregarClaves = async (claves: number) => {
+    console.log(claves);
+    const db = getDatabase();
+    let errores = 0;
+
+    for (let i = 0; i < claves; i++) {
+      try {
+        await push(ref(db, "claves/"), {
+          status: "",
+          usuario: "",
+        });
+        console.log("claves agregadas " + errores + " errores");
+        setTitulo("Claves agregadas");
+        setSubtitulo("Claves agregadas con éxito");
+        setMensaje("Se agregaron " + claves.toString() + " claves con éxito");
+        setAlerta(true);
+      } catch (error) {
+        console.log(error);
+        errores++;
+        setTitulo("Error en agregar claves");
+        setSubtitulo("Error al agregar claves");
+        setMensaje("Error al agregar " + claves.toString() + " claves");
+        setAlerta(true);
+      }
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,15 +51,11 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Zona de setso " />
+      <IonButton onClick={() => agregarClaves(5)}>Agregar Claves</IonButton>
       </IonContent>
     </IonPage>
   );
+  
 };
 
 export default Tab1;
